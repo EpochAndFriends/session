483a676aab8cf95b6749b57a64f9e839e78d8a8c
EpochAndFriends <maximilianobrocato@gmail.com>
2023-08-28 17:02:14 -0300

    socotroco6

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,7 @@
 const { validationResult } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
+
 const usersController = {
     showLoginForm: (req, res) => {
         res.render('login', { title: 'Iniciar Sesión' });
@@ -18,17 +21,30 @@ const usersController = {
                 errors: errors.array()
             });
         }
-    
+
         const { username, email, age, color, rememberColor } = req.body;
-    
+
         if (rememberColor) {
             res.cookie('selectedColor', color, { maxAge: 7 * 24 * 60 * 60 * 1000 }); // Cookie expira en 7 días
         } else {
             res.clearCookie('selectedColor');
         }
-    
+
         res.locals.bodyColor = color;
-    
+
+        // Agregar la lógica para guardar el nuevo usuario en users.json
+        const usersFilePath = path.join(__dirname, '../data/users.json'); // Ajusta la ruta aquí
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
+        const newUser = {
+            username,
+            email,
+            age,
+            color
+        };
+        users.push(newUser);
+
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 4), 'utf8');
+
         res.render('register_success', {
             title: 'Registro Exitoso',
             username,
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-    showLoginForm,
-    processLoginForm,
-    showRegisterForm,
-    processRegisterForm
-} = require('../controllers/usersController');
+const usersController = require('../controllers/usersController'); // Cambio en la importación
 const registerValidator = require('../validations/registerValidator');
 
 /* Rutas para usuarios */
 router
-    .get('/login', showLoginForm)
-    .post('/login', processLoginForm)
-    .get('/register', showRegisterForm)
-    .post('/register', registerValidator, processRegisterForm);
+    .get('/login', usersController.showLoginForm) // Cambio en la llamada a los controladores
+    .post('/login', usersController.processLoginForm) // Cambio en la llamada a los controladores
+    .get('/register', usersController.showRegisterForm) // Cambio en la llamada a los controladores
+    .post('/register', registerValidator, usersController.processRegisterForm); // Cambio en la llamada a los controladores
 
 module.exports = router;
